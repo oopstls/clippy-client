d303de956c97c08e5503df00bc7ad0768eaa00aa
oopstls <tlshoaret@gmail.com>
2025-04-17 15:46:25 +0800

    [feat] Add close menu

--- src/composables/useTray.ts
+++ src/composables/useTray.ts
@@ -3,6 +3,7 @@ import { Menu } from '@tauri-apps/api/menu';
 import { defaultWindowIcon } from '@tauri-apps/api/app';
 import { getCurrentWindow } from '@tauri-apps/api/window';
 import { exit } from '@tauri-apps/plugin-process';
+import { Dialog } from 'vant';
 
 export function useTray() {
   const showMainWindow = async () => {
@@ -13,7 +14,18 @@ export function useTray() {
 
   const handleCloseRequested = (event: any) => {
     event.preventDefault?.();
-    getCurrentWindow().hide();
+    Dialog.confirm({
+      title: '提示',
+      message: '选择窗口关闭方式',
+      confirmButtonText: '直接退出',
+      cancelButtonText: '最小化到托盘',
+    })
+      .then(() => {
+        exit(0);
+      })
+      .catch(() => {
+        getCurrentWindow().hide();
+      });
   };
 
   const initTray = async () => {
